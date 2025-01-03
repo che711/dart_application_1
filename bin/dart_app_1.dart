c1e4bb833cf7c445db4eda4b04be6d1a81fdadcd
che711 <che7111987@gmail.com>
2025-01-03 09:06:12 +0100

    added documentations lines

--- bin/dart_app_1.dart
+++ bin/dart_app_1.dart
@@ -4,4 +4,13 @@ import 'package:dart_application_1/dart_application_1.dart'
 void main(List<String> arguments) {
   print('Hello There: ${dart_application_1.calculate()}!');
   print('Hello, World: ${dart_application_1.calculate()}!');
+  10;
+  5 + 5;
+  'Dart';
+  // variable;
+
+  /*comment_one
+  comment_two
+  */
+  print('Test_msg');
 }
