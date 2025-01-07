a59a04e3ec6c5e3c39c5b1596f51005186b9babb
che711 <che7111987@gmail.com>
2025-01-07 08:06:24 +0100

    added naming rules in lesson_1.dart

--- bin/lesson_1.dart
+++ bin/lesson_1.dart
@@ -1,15 +1,27 @@
 late int tweets;
 
 void main() {
+  // declare a variable
   var number = 10;
   print(number);
   number = 20;
   print(number);
 
-  int like;
+  // declare a variable after the initialization
+  int like; // the same int like = null;
   like = 324;
   print(like);
 
+  // declare a variable with a late
   tweets = 100;
   print(tweets);
+
+  // nullable variable
+  int? comment; // не требуют инициализации перед доступом
+  print(comment);
+
+  print('test msg');
+
+  String? test_var;
+  print(test_var);
 }
