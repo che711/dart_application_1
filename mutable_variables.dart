100dd6d0e39f553640e121c6a95e13019a70c4dc
che711 <che7111987@gmail.com>
2025-01-10 08:33:25 +0100

    pdated mutable_variables.dart  1

--- mutable_variables.dart
+++ mutable_variables.dart
@@ -2,6 +2,6 @@ void main() {
   const int meYearOfBirth = 1987;
   print(meYearOfBirth);
 
-  newMeYearOfBirth = 1988;
-  print(meYearOfBirth);
+  int newMeYearOfBirth = 1988;
+  print(newMeYearOfBirth);
 }
