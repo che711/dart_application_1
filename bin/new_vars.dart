1aa8534b2790c13d07faa3f4537e3ad78117c6e0
che711 <che7111987@gmail.com>
2025-01-13 08:43:57 +0100

    added String new_vars

--- bin/new_vars.dart
+++ bin/new_vars.dart
@@ -1,5 +1,6 @@
 
 void main() {
+  print(''); // Пустая строка
   // Numbers (num -> int (целые числи) и double (числа с плавающей точкой))
   double myDouble = 10.5;
   double myDouble2 = 10;
@@ -7,11 +8,31 @@ void main() {
   int myInt = 10;
 
   print('myDouble: $myDouble, myDouble2: $myDouble2');
-  print ('myINt: $myInt'); 
+  print('myINt: $myInt');
+  print(''); // Пустая строка
 
-  // String
+  // String используется кодировка UTF-16 и это единственный тип который пошется с заглавной буквы
+  String myString = 'Hello, Dart!';
+  String myName = "Andrew";
+  String question = 'What\'s your name?'; // Экранирование символов через \
+  String myFullName = 'Andrew' + ' ' + 'Che'; // Конкатенация строк
+  String myFio = '''
+  Andrew
+  Che
+  '''; // Многострочная строка
+
+  print(myName);
+  print(myString);
+  print(question);
+  print(myFullName);
+  print(myFio);
+  print(''); // Пустая строка
 
   // Boolean
+  var a = true;
+  var b = false;
+
+  print('a: $a, b: $b');
 
   //Symmbol
 
