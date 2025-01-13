c6d276903e4c882e8cb610ee12500fadd80231c7
che711 <che7111987@gmail.com>
2025-01-13 08:38:04 +0100

    updated new_variables.dart

---
+++ bin/new_vars.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+
+void main() {
+  // Numbers (num -> int (целые числи) и double (числа с плавающей точкой))
+  double myDouble = 10.5;
+  double myDouble2 = 10;
+
+  int myInt = 10;
+
+  print('myDouble: $myDouble, myDouble2: $myDouble2');
+  print ('myINt: $myInt'); 
+
+  // String
+
+  // Boolean
+
+  //Symmbol
+
+  // Null
+
+  //List, Set, Map
+}
