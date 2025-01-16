72daa2d44a6011005652954efca6a0b59f655bdc
che711 <che7111987@gmail.com>
2025-01-16 07:41:17 +0100

    updated nullability_example.dart

--- bin/nullability_example.dart
+++ bin/nullability_example.dart
@@ -4,7 +4,13 @@ void main() {
   1. все переменные по умолчанию не могут быть null (non-nullable переменные)
   2. Fully sound. полная надежность. Dart гарантирует, что переменные, имеющие тип non-nullable,
   никогда не смогут принимать значение null (тем самым исключается падение приложения на null)*/
-   
-  String name='Andrey';
+
+  String name = 'Test_Andrey';
   print(name.toUpperCase());
+
+  String? name2;  // String? - nullable переменная (String? -> String)
+  // name2 = 'andrew';  // Flow Ananlysis. Type promotion/
+  
+  if (name2 == null) return print('Unknown name');
+  print(name2.toUpperCase());
 }
