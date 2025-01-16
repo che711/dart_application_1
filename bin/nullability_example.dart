922f05977a8093fc116d2a8aab24a4607322f65c
che711 <che7111987@gmail.com>
2025-01-16 07:26:30 +0100

    updated nullability_example.dart

--- bin/nullability_example.dart
+++ bin/nullability_example.dart
@@ -1,4 +1,9 @@
 // ignore_for_file: unnecessary_null_comparison, dead_code
 void main() {
-  
+  /* в основе null-safety лежат два принципа:
+  1. все переменные по умолчанию не могут быть null (non-nullable переменные)
+  2. */
+   
+  String name='Andrey';
+  print(name.toUpperCase());
 }
