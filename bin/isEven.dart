d6481dc03f2492e747cb0d3971a2c33856512de5
che711 <che7111987@gmail.com>
2025-01-28 08:04:37 +0100

    updated isEven.dart

--- bin/isEven.dart
+++ bin/isEven.dart
@@ -1,9 +1,13 @@
 void main() {
   // проверка на четность\нечетность числа
   num someNumber = 5;
-  print(someNumber);  // тут мы не можем вызвать .isEven, т.к. someNumber имеет тип num 
-  
+  print(
+      someNumber); // тут мы не можем вызвать .isEven, т.к. someNumber имеет тип num
+
   final someInt = someNumber as int;
   print(someNumber.isEven); // а теперь можем, т.к. someNumber имеет тип int
 
+  // перевод строки в число Styring -> int
+  var myInt = int.parse('10');
+  print(myInt);
 }
