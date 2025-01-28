a1f52c0e13726b7e0ec0d4103b651fee8cd7f3b1
che711 <che7111987@gmail.com>
2025-01-28 08:10:04 +0100

    renamed file

--- bin/type_conversion.dart
+++ bin/type_conversion.dart
@@ -8,6 +8,21 @@ void main() {
   print(someNumber.isEven); // а теперь можем, т.к. someNumber имеет тип int
 
   // перевод строки в число Styring -> int
-  var myInt = int.parse('10');
+  var myInt = int.parse('8');
   print(myInt);
+
+  // String -> double
+  var myDouble = double.parse('10.5');
+  print(myDouble);
+
+  // int -> String
+  String s1 = 10.toString();
+  print(s1);
+  String s2 = myInt.toString();
+  print(s2);
+
+  // double -> String
+  String s3 = 3.14159.toStringAsFixed(2);
+  print(s3);
+  
 }
