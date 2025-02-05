2cc3c0cadfd6ee6427e4ed0f466c46e669ebdaeb
che711 <che7111987@gmail.com>
2025-02-05 08:22:08 +0100

    updated switch_case.dart

--- bin/switch_case.dart
+++ bin/switch_case.dart
@@ -2,5 +2,22 @@
 
 void main() {
   // switch case statement
-  
+  String grade = 'B';
+
+  switch (grade) {
+    case 'A':
+      print('Excellent grade of A');
+      break;
+    case 'B':
+      print('Good');
+      break;
+    case 'C':
+      print('Fair');
+      break;
+    case 'D':
+      print('Poor');
+      break;
+    default:
+      print('Invalid grade');
+  }
 }
