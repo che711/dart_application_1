c24baa8ffdf0c6ce3b6fead7602fea18d8cd3f5a
che711 <che7111987@gmail.com>
2025-02-11 07:49:15 +0100

    added lists_and_methods.dart

---
+++ bin/lists_and_methods.dart
@@ -0,0 +1,15 @@
+void main() {
+  // create a list
+
+  // accesseing elements
+
+  // assigning values to list of elements
+
+  // adding elements to the end of the list
+
+  // inserting elements
+  
+  // removing elements
+
+  // sorting elements
+}
