62559c309b80cbc70fc29e45714e7a203c4be3cd
che711 <che7111987@gmail.com>
2025-02-11 08:05:39 +0100

    updated lists_and_methods.dart

--- bin/lists_and_methods.dart
+++ bin/lists_and_methods.dart
@@ -1,14 +1,33 @@
 void main() {
-  // create a list
+  // create a lis
+  var fruits = [
+    'apples',
+    'oranges',
+    'bananas',
+    'watermelon'
+  ]; // все в квадратных скобках литералом списка
+  fruits = [];
+  fruits = ['orange', 'pears', 'coconaut', 'melon'];
+  print(fruits);
+
+  var owoce = []; // при создании пустого списка dart думает что это список динамического типа
+  List<String> apple = ['watermelon', 'grape', 'cherr']; // указываем тип списка
+  var orange = <String>[]; // указываем тип списка
 
   // accesseing elements
+  final secondElement = fruits[1];
+  print(secondElement);
+
+  final index = fruits.indexOf('coconaut');
+  final value = fruits[index];
+  print('Значение индекса: $index будет $value');
 
   // assigning values to list of elements
 
   // adding elements to the end of the list
 
   // inserting elements
-  
+
   // removing elements
 
   // sorting elements
