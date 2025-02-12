c42802488179219df059e9b4e909c5b00e0c6861
che711 <che7111987@gmail.com>
2025-02-12 07:56:31 +0100

    updated mutabele_and_immutable_ists.dart file

--- bin/mutable_and_immutable _ists.dart
+++ bin/mutable_and_immutable _ists.dart
@@ -1,3 +1,8 @@
 void main() {
-  
+    var fruits = ['apples', 'oranges', 'bananas', 'watermelon']; // все в квадратных скобках литералом списка
+    print(fruits);
+    fruits = [];
+    print(fruits);
+    fruits = ['orange', 'pears', 'coconaut', 'melon'];
+    print(fruits);
 }
