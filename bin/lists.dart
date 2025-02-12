4758b00ec8978354f77dbe6edc381d3a86a10403
che711 <che7111987@gmail.com>
2025-02-12 08:40:04 +0100

    updated the file

---
+++ bin/lists.dart
@@ -0,0 +1,12 @@
+void main() {
+  // create a list
+  var fruits = [
+    'apples',
+    'oranges',
+    'bananas',
+    'watermelon'
+  ]; // все в квадратных скобках литералом списка
+  print(fruits);
+  print('');
+  
+}
