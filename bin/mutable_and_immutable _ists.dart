89a015e4f5c2b42f0b2d8ce659ca5f6396311f1c
che711 <che7111987@gmail.com>
2025-02-12 08:04:21 +0100

    updated the file

--- bin/mutable_and_immutable _ists.dart
+++ bin/mutable_and_immutable _ists.dart
@@ -1,5 +1,11 @@
 void main() {
     var fruits = ['apples', 'oranges', 'bananas', 'watermelon']; // все в квадратных скобках литералом списка
+    
+    /**
+    * мы не можем изменить переменную fruits через final (указывается только один раз), но можем изменить содержимое списка
+    * final fruits = ['apples', 'oranges', 'bananas', 'watermelon']; // все в квадратных скобках литералом списка
+    */
+    
     print(fruits);
     fruits = [];
     print(fruits);
