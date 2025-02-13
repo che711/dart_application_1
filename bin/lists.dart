76197900259475131a9fa6e20833e193a4755773
che711 <che7111987@gmail.com>
2025-02-13 08:18:59 +0100

    working on the lists.dart

--- bin/lists.dart
+++ bin/lists.dart
@@ -1,12 +1,25 @@
 void main() {
   // create a list
-  var fruits = [
-    'apples',
-    'oranges',
-    'bananas',
-    'watermelon'
-  ]; // все в квадратных скобках литералом списка
+  var fruits = ['apples', 'oranges', 'bananas', 'watermelon']; // все в квадратных скобках литералом списка
   print(fruits);
   print('');
+
+  print(fruits.first);
+  print('');
+
+  print(fruits.last);
+  print('');
+
+  print(fruits[0]);
+  print('');
   
+  print(fruits.length -1);
+
+  print('');
+
+  print('');
+  print('');
+
+
+
 }
