e46bd5f1f942f3c39031c5ade1c0215c7512e00d
che711 <che7111987@gmail.com>
2025-02-13 08:35:46 +0100

    added spread.dart to the bin directory

--- bin/lists.dart
+++ bin/lists.dart
@@ -1,16 +1,18 @@
 void main() {
   // create a list
-  // все в квадратных скобках литералом списка:
-  var fruits = ['apples', 'oranges', 'bananas', 'watermelon' ]; 
+  // все в квадратных скобках называется литералом списка:
+  var fruits = ['apples', 'oranges', 'bananas', 'watermelon'];
 
   // тоже самое, что и с использованием цикла for-in:
-  for(int i=0; i<fruits.length; i++){
+  print('without for-in cycle:');
+  for (int i = 0; i < fruits.length; i++) {
     final fruit = fruits[i];
     print('I like $fruit');
   }
 
+  print('for-in cycle:');
   // тоже самое, чтог и с использованием цикла for:
-  for(final fruit in fruits){
+  for (final fruit in fruits) {
     print('I like $fruit');
   }
 
---
+++ bin/spread.dart
@@ -0,0 +1,3 @@
+void main() {
+  // spread operator
+}
