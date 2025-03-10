71e358c07bce69b0193ea9ab2454faf13f60a30e
che711 <che7111987@gmail.com>
2025-03-10 07:45:24 +0100

    updated main.dart in bin directory

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,12 @@
 void main() {
-  
+  // // Используем код в одном месте
+  // var circleArea = 3.14 * 5 * 5;
+
+  // // Используем кода в дргуом месте  
+  // var circleArea = 3.14 * 5 * 5;
 }
+
+String circleArea(int radius) {
+  return 'Circle Area${3.14 * radius * radius}';
+}
+
