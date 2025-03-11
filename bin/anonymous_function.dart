f69a066317f3ad50dfdd13f41720bf361427c18d
che711 <che7111987@gmail.com>
2025-03-11 08:09:19 +0100

    updated annonimous_functions.dart

--- bin/anonymous_function.dart
+++ bin/anonymous_function.dart
@@ -1,3 +1,27 @@
 void main() {
-  
+  // // Анонимные функции - это функции без имени
+  // Function addTwoNumbers = (int a, int b) {
+  //   var sum = a + b;
+  //   print(sum);
+  // };
+
+  // Короткая запись анонимной функции
+  Function addTwoNumbers = (int a, int b) => print(a + b);
+
+  print(addTwoNumbers(2, 5));
+
+  int result = operationNumbers(10, 20, (a, b) => a * b);
+  print(result);
+}
+
+
+
+
+int operationNumbers(int a, int b, Function operation) {
+  return operation(a, b);
+}
+
+void addNumbers(int a, int b) {
+  var sum = a + b;
+  print(sum);
 }
