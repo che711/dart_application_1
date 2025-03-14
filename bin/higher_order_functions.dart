b190e090f94bd4079434e330566cb96a365bf194
che711 <che7111987@gmail.com>
2025-03-14 07:31:48 +0100

    updated bin/higher_order_functions.dart_2

--- bin/higher_order_functions.dart
+++ bin/higher_order_functions.dart
@@ -4,7 +4,39 @@ void main() {
   //reduce  1 2 3 4 5 6 => 21
 
   const listNumbers = [1, 2, 3, 4, 5];
+
+  final doubleElement = (number) => print(2 * number);
+  listNumbers.forEach(doubleElement);
+
   listNumbers.forEach((int number) {
     print(2 * number);
   });
+  print('');
+
+  // map
+  final carColor = {
+    'Ferrari': 'red',
+    'Lamborghini': 'black',
+    'Porsche': 'white',
+  };
+  
+  carColor.forEach((car, color) => print('$car is $color'));
+  print('');
+
+  final looped = <int>[];
+  for (final x in listNumbers) {
+    looped.add(x * x);
+  }
+  print(looped);
+  print('');
+
+  // map
+  final mapped = listNumbers.map((x) => x * x);
+  print(mapped);
+  print('');  
+
+  // where
+  final odds = listNumbers.where((x) => x.isOdd);
+  print(odds);
+  print('');
 }
