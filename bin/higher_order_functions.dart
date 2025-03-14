74162cf4df804e4e15afcf001bfe2a1da9dbf5f1
che711 <che7111987@gmail.com>
2025-03-14 07:15:29 +0100

    updated bin/higher_order_functions.dart

--- bin/higher_order_functions.dart
+++ bin/higher_order_functions.dart
@@ -3,5 +3,8 @@ void main() {
   //where   1 2 3 4 5 6 => 1 3 5
   //reduce  1 2 3 4 5 6 => 21
 
-  
+  const listNumbers = [1, 2, 3, 4, 5];
+  listNumbers.forEach((int number) {
+    print(2 * number);
+  });
 }
