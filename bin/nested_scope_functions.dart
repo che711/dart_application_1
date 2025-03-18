c2a0f33eaff23f78349edbaedf3acfe46719c60e
che711 <che7111987@gmail.com>
2025-03-18 08:16:32 +0100

    updated nested_scope_functions.dart_2

--- bin/nested_scope_functions.dart
+++ bin/nested_scope_functions.dart
@@ -1,10 +1,31 @@
-void main(){
-    void myFunction(){
-        print('Inside myFunction');
-        void nestedFunction(){
-            print('Inside nestedFunction');
-        }
-        nestedFunction();
+// вложенные функции имеют смысл если мы планируем испльзовать
+//некие повторяющиеся действия только внутри одной функции
+
+// Области видиомсти переменных - функция nestedFunction видит все переменные
+// объявленные во всех функциях выше нее
+// topLevelVariable - глобальная переменная доступна везде
+
+var topLevelVariable = 'top level variable';
+
+void main() {
+  String variableInsideMain = 'variable inside main';
+
+  void myFunction() {
+    String variableInsideMyFunction = 'variable inside myFunction';
+    print('Inside myFunction');
+
+    void nestedFunction() {
+      String variableInsideNestedFunction = 'variable inside nestedFunction';
+      print('');
+      print(topLevelVariable);
+      print(variableInsideMain);
+      print(variableInsideMyFunction);
+      print('');
+      print('Inside nestedFunction');
     }
-    myFunction();
+
+    nestedFunction();
+  }
+
+  myFunction();
 }
