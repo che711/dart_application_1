95650c64897cf6e4a219102b563bbdf7a14029fc
che711 <che7111987@gmail.com>
2025-03-25 08:03:06 +0100

    updated classes_and_objects.dart

--- bin/classes_and_objects.dart
+++ bin/classes_and_objects.dart
@@ -1,4 +1,14 @@
 void main() {
-  // tbd
-  
+  final myCar = Car();
+  myCar.vin = 503;
+  myCar.model = 'BMW';
+
+  print(myCar);
+}
+
+// класс Car должен выше или ниже метода main
+
+class Car {
+  int? vin = 0;
+  String model = '';
 }
