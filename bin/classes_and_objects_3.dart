7a2936eebe402b1f865258523db40c78a5752637
che711 <che7111987@gmail.com>
2025-04-02 08:09:10 +0200

    updated new dart file

--- bin/classes_and_objects_3.dart
+++ bin/classes_and_objects_3.dart
@@ -1,15 +1,30 @@
+late int global;
+
 void main() {
-  var pizza = Pizza();
+
+  global = 10;
+  print('Print global late int: $global');
+
+
+  var pizza = Pizza().orgerPizza();
+  // pizza.price = 20;
   print(pizza);
 }
 
 class Pizza {
   String name;
-  double price;
+  final double price;
 
   Pizza()
       : name = 'Spring',
         price = 10.50;
+
+  // Pizza(this.name, this.price);
+
+  String orgerPizza() {
+    print('Cooking pizza...');
+    return 'Your Pizza: $name, price: $price is ready';
+  }
 
   @override
   String toString() => 'Pizza: $name, price: $price';
