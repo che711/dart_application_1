31d8fa228eea9fef3a76bc9791d973a90f59ca89
che711 <che7111987@gmail.com>
2025-04-14 08:33:43 +0200

    updated oop.dart

--- bin/oop.dart
+++ bin/oop.dart
@@ -5,15 +5,34 @@
 // Иерархическое наследование - это когда один класс наследует от другого класса, который сам наследует от нескольких классов (ClassA -> ClassС, ClassA)
 // Множественное наследование - это когда один класс наследует от нескольких классов (ClassA -> ClassB + ClassC)
 
-void main() {}
+void main() {
+  final konstantin = Person('John', 'Doe');
+  final ivan = Student(
+    'Ivan',
+    'Doe',
+  );
+  konstantin.gitFullName();
+  ivan.grabed.add(5);
+  ivan.gitFullName();
+
+}
 
 class Person {
   String name;
   String surname;
 
   Person(this.name, this.surname);
-  void display() 
-
+  void gitFullName() => print('$name $surname');
 }
 
-class Student extends Person {}
+class Student extends Person {
+  List<int> grabed = [];
+
+  Student(super.name, super.surname);
+
+  @override
+  void gitFullName() {
+    super.gitFullName();
+    print('Student has next grabes: $grabed');
+  }
+}
