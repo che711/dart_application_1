d39f79592342acee624511ef6d342d20c4050885
che711 <che7111987@gmail.com>
2025-04-22 08:12:04 +0200

    updated generics.dart

--- bin/generics.dart
+++ bin/generics.dart
@@ -1,14 +1,27 @@
+// generics - помогают избежать дупликацию кода и обеспечивают безопасность типов
 void main() {
-  Car car = Car(1, "BMW");
+  Car car = Car('2', "BMW");
   print(car.id.runtimeType);
+
+  // int id = car.id;
+  // print(id);
+
+  print("");
+  Car car1 = Car(1, 'Ford');
+  print(car1.id.runtimeType);
 }
 
-class Car {
-  int id;
+class Car<T> {
+  // <T> - Тип
+  // <E> - Элемент
+  // <K> - Ключ
+  // <V> - Значение
+
+  T id;
   String carName;
 
   Car(this.id, this.carName);
-  void move(){
+  void move() {
     print('The $carName. ID number: $id');
   }
 }
