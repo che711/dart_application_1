e0a069f567a5248a0ef0517c5476a38c30f7af1d
che711 <che7111987@gmail.com>
2025-04-23 07:52:57 +0200

    updated all project

--- bin/nullsafety.dart
+++ bin/nullsafety.dart
@@ -1,5 +1,15 @@
-void main() {
-  print('Hello, World!');
+// sound Null Safety
+// Dart - типобезопасный язык (type-safe)
 
-  print('');
+// В основе Null Safety лежит 3 принципа:
+// 1. Non-nullabel by default 
+// 2. Fully sound - полная надежность
+// 3. Incrementally adoptable - поэтапная миграция
+
+void main() {
+  int? age = 2;  // can be nullable 
+
+  String name = 'Mark';
+  print(name);
+  print(age);
 }
