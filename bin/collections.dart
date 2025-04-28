767e28a66e83eaaefd9a4548eae5a0eacbe1ac90
che711 <che7111987@gmail.com>
2025-04-28 07:39:45 +0200

    updated bin/collections.dart

--- bin/collections.dart
+++ bin/collections.dart
@@ -3,6 +3,18 @@
 // ?[] - null-aware оператор доступа по индексу
 
 void main() {
-  List<String> nullableValues = ['Warsaw', 'LA', 'NY'];
+  List<String?> nullableValues = [null, 'Warsaw', null, 'LA', 'NY'];
+  List<String>? nullableList;
   print(nullableValues);
+
+  print('');
+  print(nullableList?[0]);
+
+  print("");
+  nullableValues[0] ??= 'SPB';
+  print(nullableValues[0]);
+
+  print("");
+  String someStirng = nullableValues[2] ?? 'Vologda';
+  print(someStirng);
 }
