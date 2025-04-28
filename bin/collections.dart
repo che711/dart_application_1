ec8182e4345de63fdbed9e2395ff0412b61e3a2b
che711 <che7111987@gmail.com>
2025-04-28 07:46:27 +0200

    updated bin/collections.dart_2

--- bin/collections.dart
+++ bin/collections.dart
@@ -3,6 +3,9 @@
 // ?[] - null-aware оператор доступа по индексу
 
 void main() {
+  Map<String, int> categories = {'kitchen': 1, 'bedroom': 2};
+  print(categories);
+
   List<String?> nullableValues = [null, 'Warsaw', null, 'LA', 'NY'];
   List<String>? nullableList;
   print(nullableValues);
