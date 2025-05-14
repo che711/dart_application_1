1770406a651f0711bb6c9ca54ad8c6a7c5acbcb1
che711 <che7111987@gmail.com>
2025-05-14 07:54:03 +0200

    updated future.dart_2

--- bin/future_api.dart
+++ bin/future_api.dart
@@ -2,9 +2,19 @@ void main() {
   print('');
 
   // Класс Future<T>, где Т - тип возвращаемого результата
-  Future.delayed(Duration(seconds: 3), () {
+  final myFuture = Future.delayed(Duration(seconds: 3), () {
     print('Future начал выполняться');
-    return 'Данные успешно загружены';
+    // return 'Данные успешно загружены';
+    throw 'Сервер не отвечает';
   });
+
+  myFuture.then((result) => print('Результат: $result'))
+  .catchError((error)=>print('Ошибка: $error'),
+  test: (error)=> error.runtimeType == String,
+  )
+  .whenComplete(()=>print('Future завершил свою работу'));
+
+  print(myFuture);
+
   print('Завершение работы функции main()');
 }
