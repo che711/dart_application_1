72618b578ff91b3c6895536dfdd6d0daa5e914b8
che711 <che7111987@gmail.com>
2025-05-15 08:30:49 +0200

    updated stream.dart

--- bin/stream.dart
+++ bin/stream.dart
@@ -3,8 +3,20 @@
 //   2. с помощью асинхронного генератора
 //   3. с помощью StreamController
 
+import 'dart:async';
+
+StreamSubscription<int>? subscription;
 
 void main() {
-  
+  final stream = Stream.periodic(Duration(seconds: 1), (tick) => tick);
+
+  stream.listen((data) {
+    print('Получено: $data');
+  });
+
+  Future.delayed(Duration(seconds: 5), () {
+    print('Отписка от потока');
+    subscription?.cancel();
+  });
 }
 
